82d5399d92a11533ad1a62e266f541ffa57bd06f
edwinr196 <112092352+edwinr196@users.noreply.github.com>
2022-09-29 14:07:44 -0600

    Add files via upload

---
+++ lab05-ejercicio1.py
@@ -0,0 +1,11 @@
+
+from textwrap import shorten
+
+
+try:
+    entrada = input("Ingrese el nombre del archivo: ")
+    archivo = open(entrada, "r", encoding="UTF-8")
+    for linea in archivo:
+        print(linea.upper())
+except:
+    print("Error, archivo no existe")
